51f03b512a983faa50cb6b32ed48017d5b3548c6
john <john@jpatmore.plus.com>
2021-08-12 00:49:39 +0100

    Alter a single position in the game cube

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
-
-
-# TODO: Define the matrix representing the game boundaries
-# A position is defined by three coordinates, [x, y, z]
+# Define the matrix representing the game boundaries
+# A position is defined by three coordinates:
+#     [board, row, column] = [up-down, front-back, left-right] from a front view
 # A row is 4 locations
 # A board ia 4 rows forming a 4 x 4 grid
 # A cube is 4 boards stacked one on top of another
-position = [0, 0, 0]
-row = [position] * 4
-board = [row] * 4
-cube = [board] * 4
+# row = [
+#     0,
+#     0,
+#     0,
+#     0
+# ]
+# board = [
+#     [0, 0, 0, 0],
+#     [0, 0, 0, 0],
+#     [0, 0, 0, 0],
+#     [0, 0, 0, 0]
+# ]
+cube = [
+    #   row 0         row 1         row 2         row 3
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],  # board 0
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],  # board 1
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],  # board 2
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]   # board 3
+]
 
-print(f'{position = }')
-print(f'{row = }')
-print(f'{board = }')
+# Change the value of a single position in the cube
+x = 3  # 4th position right
+y = 2  # 3rd board down
+z = 1  # 2nd row back
+
+cube[y][z][x] = 2
 print(f'{cube = }')
 
 """
-position = [0, 0, 0]
-row = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-board = [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]
-cube = [[[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]]
-
+cube = [
+    #   row 0         row 1         row 2         row 3
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],  # board 0
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],  # board 1
+    [[0, 0, 0, 0], [0, 0, 0, 2], [0, 0, 0, 0], [0, 0, 0, 0]],  # board 2
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]   # board 3
+]
 """
 
-
 # TODO: Define the winning combinations
+
 
 # TODO: Draw the board (2D)
 
