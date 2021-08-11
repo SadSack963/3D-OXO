ab97c38f6cc3f0599f0175837d89df0daea2a1fb
john <john@jpatmore.plus.com>
2021-08-11 23:05:49 +0100

    Game layout matrices defined

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+# TODO: Define the matrix representing the game boundaries
+# A position is defined by three coordinates, [x, y, z]
+# A row is 4 locations
+# A board ia 4 rows forming a 4 x 4 grid
+# A cube is 4 boards stacked one on top of another
+position = [0, 0, 0]
+row = [position] * 4
+board = [row] * 4
+cube = [board] * 4
+
+print(f'{position = }')
+print(f'{row = }')
+print(f'{board = }')
+print(f'{cube = }')
+
+"""
+position = [0, 0, 0]
+row = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+board = [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]
+cube = [[[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]], [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]]
+
+"""
+
+
+# TODO: Define the winning combinations
+
+# TODO: Draw the board (2D)
+
+# TODO: Get user1 input
+
+# TODO: Draw the user1 choice on the board
+
+# TODO: Detect a win
+
+# TODO: Get user2 input
+
+# TODO: Draw the user2 choice on the board
+
+# TODO: Detect a win
